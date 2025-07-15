eacc04e6ffc5d8ea26e4d28c439df60867148525
Finn Ellis <finn@finn.fun>
2025-07-14 20:43:17 -0700

    double reset settings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -86,6 +86,7 @@ function App() {
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (doubleReset) {
       setField(getEmptyField);
+      setSettings(defaultSettings);
     } else {
       setField(nextField);
     }
